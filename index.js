bf67f97f3fa180901a662055bc3e7fe4ffec8858
Sergio Muñoz <andressam2031@gmail.com>
2022-12-19 22:30:50 -0500

    Funciones de encriptar  desencriptar resueltas

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
 window.addEventListener("load", ()=>{
+
+    function eliminar(texto, eliminar, nuevo){
+        let txt = texto;
+        while(txt.includes(eliminar)){
+            txt = txt.replace(eliminar,nuevo);
+        }
+        return txt;
+    }
     
+    function encriptar(texto){
+
+        texto = texto.split("");
+        let textoEncriptado = "";
+
+        texto.forEach(letra => {
+            switch(letra){
+                case "a":
+                    textoEncriptado += "ai";
+                    break;
+                case "e":
+                    textoEncriptado += "enter";
+                    break;
+                case "i":
+                    textoEncriptado += "imes";
+                    break;
+                case "o":
+                    textoEncriptado += "ober";
+                    break;
+                case "u":
+                    textoEncriptado += "ufat";
+                    break;
+                default:
+                    textoEncriptado += letra;
+                    break;
+            }
+        });
+
+        return textoEncriptado;
+    }
+
+    function desencriptar(texto){
+
+        let textoDesencriptado = texto;
+
+        textoDesencriptado = eliminar(textoDesencriptado,"ai","a");
+        textoDesencriptado = eliminar(textoDesencriptado,"enter","e");
+        textoDesencriptado = eliminar(textoDesencriptado,"imes","i");
+        textoDesencriptado = eliminar(textoDesencriptado,"ober","o");
+        textoDesencriptado = eliminar(textoDesencriptado,"ufat","u");
+        
+        return textoDesencriptado;
+    }
+
+
 });
